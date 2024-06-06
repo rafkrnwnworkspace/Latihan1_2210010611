1a4cd90dbaff53400ac25a8ce0ef4637e7501621
rafkrnwnworkspace <rafkrnwn@gmail.com>
2024-06-06 12:46:15 +0800

    Melengkapi method Matematika

--- src/hitung/matematika.java
+++ src/hitung/matematika.java
@@ -8,9 +8,23 @@ public class matematika {
     public matematika(double bil1, double bil2){
         this.bil1 = bil1;
         this.bil2 = bil2;
+        
     }
     
-    double setPenjumlahan(){
+    public double setPenjumlahan(){
         return bil1 + bil2;
+    }
     
+    public double setPengurangan(){
+        return bil1 - bil2;
+
+    }
+    
+    public double setPerkalian(){
+        return bil1 * bil2;
+    }
+    
+    public double setPembagian(){
+        return bil1 * bil2;
+    }
 }
